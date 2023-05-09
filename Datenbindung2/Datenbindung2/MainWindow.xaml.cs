560653c481dda0dfa4cd20cb4f6e496b1f6f2f50
drjofu <drjofu@web.de>
2023-05-09 11:59:34 +0200

    Tag 2

---
+++ Datenbindung2/Datenbindung2/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Datenbindung2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+      this.DataContext = new Firma();
+    }
+
+    private void AlterAnpassen(object sender, RoutedEventArgs e)
+    {
+      var liste = ((Firma)DataContext).Mitarbeiter;
+      foreach (var person in liste)
+      {
+        person.Alter++;
+      }
+    }
+
+    private void NeuerMitarbeiter(object sender, RoutedEventArgs e)
+    {
+      var liste = ((Firma)DataContext).Mitarbeiter;
+      liste.Add(new() { Name = "Der Neue", Alter = 21, Wohnort = "hier" });
+    }
+  }
+}
---
+++ Datenbindung2/Datenbindung2/Person.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Datenbindung2
+{
+  class Person : INotifyPropertyChanged
+  {
+    public string Name { get; set; }
+    public string Wohnort { get; set; }
+    //public int Alter { get; set; }
+
+    private int alter;
+
+    public int Alter
+    {
+      get { return alter; }
+      set
+      {
+        if (alter == value) return;
+
+        alter = value;
+        OnPropertyChanged();
+        OnPropertyChanged(nameof(IstErfahren));
+      }
+    }
+
+    protected void OnPropertyChanged([CallerMemberName]string propertyName = null)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    public bool IstErfahren => Alter > 50;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+  }
+
+  class Firma
+  {
+    public string Name { get; set; } = "Hinz & Kunz";
+    public ObservableCollection<Person> Mitarbeiter { get; set; } = new()
+    {
+      new Person(){ Name="Dagobert", Wohnort="Entenhausen", Alter=77},
+      new Person(){ Name="Marie", Wohnort="Köln", Alter=33},
+      new Person(){ Name="Petra", Wohnort="Ulm", Alter=44},
+      new Person(){ Name="Karl", Wohnort="München", Alter=65},
+      new Person(){ Name="Carla", Wohnort="Berlin", Alter=56}
+    };
+  }
+}
