ce308fd3aff11682963552c942cd0496a24a39a1
drjofu <drjofu@web.de>
2023-05-10 13:25:13 +0200

    Tag 3

---
+++ MVVMMini/MVVMMini/ActionCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMMini
+{
+  public class ActionCommand : ICommand
+  {
+    public string DisplayText { get; set; }
+    public string ToolTipText { get; set; }
+
+    private readonly Action action;
+
+    public ActionCommand(Action action)
+    {
+      this.action = action;
+    }
+
+    private bool isEnabled = true;
+
+    public bool IsEnabled
+    {
+      get { return isEnabled; }
+      set
+      {
+        isEnabled = value;
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+      }
+    }
+
+    // ICommand implementation
+    public event EventHandler? CanExecuteChanged;
+
+    public bool CanExecute(object? parameter)
+    {
+      return isEnabled;
+    }
+
+    public void Execute(object? parameter)
+    {
+      action?.Invoke();
+    }
+  }
+}
